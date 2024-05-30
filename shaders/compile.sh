401929d6f00f9a150ce862e71dd3044d6dd24394
Roberto Leone Cicognani <82660558+robertoleonecicognani@users.noreply.github.com>
2024-05-30 10:54:14 +0200

    Add files via upload

---
+++ shaders/compile.sh
@@ -0,0 +1,6 @@
+glslc shader.frag -o frag.spv
+glslc shader.vert -o vert.spv
+glslc skybox.frag -o skyboxFrag.spv
+glslc skybox.vert -o skyboxVert.spv
+glslc text.frag -o textFrag.spv
+glslc text.vert -o textVert.spv 
